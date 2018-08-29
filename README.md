52b67a6cb7f69d7632aae11ab0ae4e88a253643e
xiyangyangyuwo <18332712352@163.com>
2018-08-29 11:01:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 基于Vue-cli Swiper的项目
+
+> A project
+
+## Build Setup
+
+``` bash
+# 安装项目依赖
+npm install
+
+# 项目运行在 localhost:8080
+npm run dev
+
+# 打包编译
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  @import '../static/iconfont/iconfont.css';
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  ul {
+    list-style: none;
+  }
+</style>
